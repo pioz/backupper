ccb8e48fb8783dc1b6ad395bb6935e4880ffa27f
pioz <epilotto@gmx.com>
2024-02-02 15:22:36 +0100

    Avoid use database and create database statements on mysql dump

--- lib/backupper/dump_command.rb
+++ lib/backupper/dump_command.rb
@@ -1,18 +1,16 @@
 module DumpCommand
   def self.mysql(database:, username: 'root', password: nil, dump_options: nil, outfile:)
     params = []
-    params << "--databases '#{database}'"
     params << "-u'#{username}'"
     params << "-p'#{password}'" if password
     params << dump_options if dump_options
-    return "mysqldump #{params.join(' ')} | bzip2 > '#{outfile}'"
+    return "mysqldump '#{database}' #{params.join(' ')} | bzip2 > '#{outfile}'"
   end
 
   def self.postgresql(database:, username: 'root', password: nil, dump_options: nil, outfile:)
     params = []
     params << "-U '#{username}'"
-    params << "'#{database}'"
     params << dump_options if dump_options
-    return "PGPASSWORD='#{password}' pg_dump #{params.join(' ')} | bzip2 > '#{outfile}'"
+    return "PGPASSWORD='#{password}' pg_dump '#{database}' #{params.join(' ')} | bzip2 > '#{outfile}'"
   end
 end
